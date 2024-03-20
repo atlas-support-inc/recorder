b8e6f78afe8aa94f576ac7239f2aeb5057da24a8
Grigory <grigory@atlas.so>
2024-03-20 18:00:00 +0400

    Allow seeking while skipping inactive, tweak fast_forward speed

--- packages/rrweb/typings/plugins/console/record/stringify.d.ts
+++ packages/rrweb/typings/plugins/console/record/stringify.d.ts
@@ -1,2 +1,2 @@
-import { StringifyOptions } from './index';
-export declare function stringify(obj: any, stringifyOptions?: StringifyOptions): string;
+import type { StringifyOptions } from './index';
+export declare function stringify(obj: unknown, stringifyOptions?: StringifyOptions): string;
